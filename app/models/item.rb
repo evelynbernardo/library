940ea3896a6736f0824504ef9cef34399441679a
evelynbernardo <bernardoevelynbarros@gmail.com>
2015-02-25 05:08:25 +0000

    f

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ActiveRecord::Base
   belongs_to :item_type
-
+  validates :title, presence: true
+  
   def completed?
-  	if completed_on != nil
-  		return true
-  	else
+  	if completed_on == nil
   		return false
+  	else
+  		return true
   	end
   end
 end
